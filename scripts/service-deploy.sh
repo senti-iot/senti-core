d12ec4f49ba2e6aafe3e80376dc5e1ed0c3dbb6f
Andrei Tudor <andrei.tudor27@gmail.com>
2020-01-15 12:49:08 +0100

    Core

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-if [[ "$1" == "master" ]]; then 
+if [[ "$1" == "master" ]]; then
 	echo
-	echo Deploying Senti Gateway $1 ... 
-	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/gateway/production
+	echo Deploying Senti core $1 ...
+	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/core/production
 	echo
-	echo Restarting Senti Gateway service: $1 ... 
-	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/gateway/production/scripts/service-restart.sh master'
+	echo Restarting Senti core service: $1 ...
+	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/core/production/scripts/service-restart.sh master'
 	echo
-	echo Deployment to Senti Gateway $1 and restart done!
+	echo Deployment to Senti core $1 and restart done!
 	exit 0
-fi 
+fi
 
-if [[ "$1" == "dev" ]]; then 
+if [[ "$1" == "dev" ]]; then
 	echo
-	echo Deploying Senti Gateway $1 ... 
-	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/gateway/development
+	echo Deploying Senti core $1 ...
+	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/core/development
 	echo
-	echo Restarting Senti Gateway service: $1 ... 
-	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/gateway/development/scripts/service-restart.sh dev'
+	echo Restarting Senti core service: $1 ...
+	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/core/development/scripts/service-restart.sh dev'
 	echo
-	echo Deployment to Senti Gateway $1 and restart done!
+	echo Deployment to Senti core $1 and restart done!
 	exit 0
 fi
--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # chmod 700 api-restart.sh
 
-if [[ "$1" == "master" ]]; then 
-	npm install --prefix /srv/nodejs/senti/services/gateway/production
-	systemctl restart senti-gateway.service
+if [[ "$1" == "master" ]]; then
+	npm install --prefix /srv/nodejs/senti/services/core/production
+	systemctl restart senti-core.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway MASTER updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti core MASTER updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
 
-if [[ "$1" == "dev" ]]; then 
-	npm install --prefix /srv/nodejs/senti/services/gateway/development
-	systemctl restart senti-gateway-dev.service
+if [[ "$1" == "dev" ]]; then
+	npm install --prefix /srv/nodejs/senti/services/core/development
+	systemctl restart senti-core-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway DEV updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti core DEV updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
