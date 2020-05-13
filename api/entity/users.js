ebb657cdf9898377fd6b85b08416a2e9ba183694
Mikkel Broberg <mb@webhouse.dk>
2020-05-13 14:26:40 +0200

    Fix get org users

--- api/entity/users.js
+++ api/entity/users.js
@@ -7,6 +7,9 @@ const aclClient = require('../../server').aclClient
 const Privilege = require('../../lib/acl/dataClasses/Privilege')
 const ResourceType = require('../../lib/acl/dataClasses/ResourceType')
 
+/**
+ * Get all users the lease has access to.
+ */
 router.get('/v2/entity/users', async (req, res) => {
 	let lease = await authClient.getLease(req)
 	if (lease === false) {
@@ -16,20 +19,23 @@ router.get('/v2/entity/users', async (req, res) => {
 	let resources = await aclClient.findResources(lease.uuid, '00000000-0000-0000-0000-000000000000', ResourceType.user, Privilege.user.read)
 	let entity = new entityService()
 	let queryUUIDs = (resources.length > 0) ? resources.map(item => { return item.uuid }) : false
-	console
 	res.status(200).json(await entity.getUsersByUUID(queryUUIDs))
 })
-
-router.get('/v2/entity/users/:uuid', async (req, res) => {
+/**
+ * Get all users in :orguuid ord, that lease user has access to.
+ * @routeparam {String} :orguuid 
+ */
+router.get('/v2/entity/users/:orguuid', async (req, res) => {
 	let lease = await authClient.getLease(req)
 	if (lease === false) {
 		res.status(401).json()
 		return
 	}
 	let entity = new entityService()
-	let org = await entity.getDbOrganisationByUUID(req.params.uuid)
-	let aclOrg = await entity.getAclOrgResourcesOnName(org.id)
-	let resources = await aclClient.findResources(lease.uuid, aclOrg.aclorg.uuid, ResourceType.user, Privilege.user.read)
+	// Add if we want recursive under ACL ORG
+	// let org = await entity.getDbOrganisationByUUID(req.params.uuid)
+	// let aclOrg = await entity.getAclOrgResourcesOnName(org.id)
+	let resources = await aclClient.findResources(lease.uuid, req.params.orguuid, ResourceType.user, Privilege.user.read) // aclOrg.aclorg.uuid
 	let queryUUIDs = (resources.length > 0) ? resources.map(item => { return item.uuid }) : false
 	res.status(200).json(await entity.getUsersByUUID(queryUUIDs))
 })
