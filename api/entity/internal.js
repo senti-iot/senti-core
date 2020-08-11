114c8d055b1b22afb8cf4550acf1c69bf9716e1e
Mikkel Broberg <mb@webhouse.dk>
2020-08-11 14:14:03 +0200

    root, org and user acl fix

--- api/entity/internal.js
+++ api/entity/internal.js
@@ -18,6 +18,7 @@ const aclClient = require('../../server').aclClient
 
 const Privilege = require('../../lib/acl/dataClasses/Privilege')
 const ResourceType = require('../../lib/acl/dataClasses/ResourceType')
+const { exit } = require('process')
 
 const createAPI = require('apisauce').create
 
@@ -99,6 +100,260 @@ router.post('/v1/internal/mail/send', async (req, res) => {
 
 	let sendMail = new sendMail()
 	let result = await sendMail.send(req.body)
+	res.status(200).json(result)
+})
+
+router.post('/entity/organisation/initaclroot', async (req, res) => {
+	let entity = new entityService()
+	
+	let org = await entity.getRootOrganisation()
+	let aclResources = await entity.getAclResources()
+	if (aclResources === false) {
+		let aOrgResources = [1, 2, 3, 5, 7, 8, 14]
+		await Promise.all(Object.entries(ResourceType).map(async ([key, type]) => {
+			await entity.dbSaveAclResource({ "name": key, "type": type, internal: { isOrgResource: aOrgResources.includes(type) } })
+		}))
+		aclResources = await entity.getAclResources()
+	}
+	// Register ACLORG ON ROOT as 00000000-0000-0000-0000-000000000000
+	await entity.dbSaveAclOrgResource(org.id, aclResources.filter(r => r.type === 1)[0].id, '00000000-0000-0000-0000-000000000000')
+	let aclOrgResources = await entity.createAclOrgResources(org)
+
+	// Register ACL ORG as resource
+	let aclOrgResource = await aclClient.registerResource(aclOrgResources.aclorg.uuid, ResourceType.aclorg)
+	// Register org as entity
+	let orgEntity = await aclClient.registerEntity(org.uuid)
+	// Register org as resource under ACL ORG
+	let orgResource = await aclClient.registerResource(org.uuid, ResourceType.org)
+	if (orgEntity.uuid !== orgResource.uuid) {
+		console.log('Something went really wrong...')
+	}
+	await aclClient.addResourceToParent(orgResource.uuid, aclOrgResource.uuid)
+
+	// Register aclOrgResources and add them to ORG
+	await Promise.all(Object.entries(aclOrgResources).map(async ([, aclResource]) => {
+		if (aclResource.type !== 1 && aclResource.type !== 3) {
+			await aclClient.registerResource(aclResource.uuid, aclResource.type)
+			await aclClient.addResourceToParent(aclResource.uuid, orgResource.uuid)
+		}
+	}))
+	
+	// Check for ROLES or create some...
+	// let roles = await entity.getRoles()
+	// if (roles === false) {
+	// 	await localCreateRoles()
+	// 	roles = await entity.getRoles()
+	// }
+
+	// Check for ORGROLES or create some...
+	let orgRoles = await entity.createAclOrganisationRoles(org.id)
+	console.log(orgRoles)
+	await Promise.all(orgRoles.map(async (orgRole) => {
+		// Register role as entity under org
+		await aclClient.registerEntity(orgRole.aclUUID)
+		await aclClient.addEntityToParent(orgRole.aclUUID, orgEntity.uuid)
+		// Add initial privileges for role on org->aclResources
+		await Promise.all(Object.entries(orgRole.internal.initialPrivileges).map(async ([key, privileges]) => {
+			let p = await aclClient.addPrivileges(orgRole.aclUUID, aclOrgResources[key].uuid, privileges)
+			//console.log(orgRole.uuid, aclOrgResources[key].uuid, privileges, p)
+		}))
+	}))
+	//console.log(orgRoles.filter(role => { return role.roleId === 1 })[0])
+
+	// ADD USERS administrator and system to root aclorg
+	// let administrator = await entity.getUserByUserName('administrator')
+
+	// if (administrator === false) {
+	// 	let requestUser = new RequestUser()
+	// 	requestUser.userName = 'administrator'
+	// 	requestUser.firstName = 'System'
+	// 	requestUser.lastName = 'Administrator'
+	// 	requestUser.email = 'support@senti.cloud'
+	// 	requestUser.state = 0
+	// 	requestUser.orgId = org.id
+	// 	requestUser.role = orgRoles.filter(role => { return role.roleId === 2 })[0]
+	// 	requestUser.roleId = requestUser.role.roleId
+	// 	administrator = await entity.createUser(requestUser)
+	// }
+	
+	// await aclClient.registerEntity(administrator.uuid)
+	// await aclClient.addEntityToParent(administrator.uuid, orgRoles.filter(role => { return role.roleId === 2 })[0].aclUUID)
+
+	// await aclClient.registerResource(administrator.uuid, ResourceType.user)
+	// await aclClient.addResourceToParent(administrator.uuid, aclOrgResources.users.uuid)
+
+	// let dbAdm = await entity.getDbUserByUUID(administrator.uuid)
+	// let admPass = new RequestCredentials({ id: dbAdm.id, newPassword: req.body.admpass })
+	// await entity.setUserPassword(admPass)
+
+	// let systemuser = await entity.getUserByUserName('system')
+
+	// if (systemuser === false) {
+	// 	let requestSystemUser = new RequestUser()
+	// 	requestSystemUser.userName = 'system'
+	// 	requestSystemUser.firstName = 'System'
+	// 	requestSystemUser.lastName = 'Administrator'
+	// 	requestSystemUser.email = 'support@senti.cloud'
+	// 	requestSystemUser.state = 0
+	// 	requestSystemUser.orgId = org.id
+	// 	requestSystemUser.role = orgRoles.filter(role => { return role.roleId === 1 })[0]
+	// 	requestSystemUser.roleId = requestSystemUser.role.roleId
+	// 	systemuser = await entity.createUser(requestSystemUser)
+	// }
+	// await aclClient.registerEntity(systemuser.uuid)
+	// await aclClient.addEntityToParent(systemuser.uuid, orgRoles.filter(role => { return role.roleId === 1 })[0].aclUUID)
+
+	// await aclClient.registerResource(systemuser.uuid, ResourceType.user)
+	// await aclClient.addResourceToParent(systemuser.uuid, aclOrgResources.users.uuid)
+
+	// let dbSys = await entity.getDbUserByUUID(systemuser.uuid)
+	// let sysPass = new RequestCredentials({ id: dbSys.id, newPassword: req.body.syspass })
+	// await entity.setUserPassword(sysPass)
+	res.status(200).json(org)
+})
+
+router.get('/v2/internal/organisation/allaclfix', async (req, res) => {
+	let lease = await authClient.getLease(req)
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+	let entity = new entityService()
+
+	let select = `SELECT *
+	FROM (
+		SELECT O1.id, 0 as parentId, 1 as niveau
+		FROM sentidatastorage_dev.organisation O1
+		WHERE O1.parentOrgId=0
+	) o1
+	UNION
+	SELECT *
+	FROM (
+		SELECT O2.id, O2.parentOrgId as parentId, 2 as niveau
+		FROM sentidatastorage_dev.organisation O1
+		INNER JOIN sentidatastorage_dev.organisation O2 ON O2.parentOrgId=O1.id
+		WHERE O1.parentOrgId=0
+	) o2
+	UNION
+	SELECT *
+	FROM (
+		SELECT O3.id, O3.parentOrgId as parentId, 3 as niveau
+		FROM sentidatastorage_dev.organisation O1
+		INNER JOIN sentidatastorage_dev.organisation O2 ON O2.parentOrgId=O1.id
+		INNER JOIN sentidatastorage_dev.organisation O3 ON O3.parentOrgId=O2.id
+		WHERE O1.parentOrgId=0
+	) o3
+	UNION
+	SELECT *
+	FROM (
+		SELECT O4.id, O4.parentOrgId as parentId, 4 as niveau
+		FROM sentidatastorage_dev.organisation O1
+		INNER JOIN sentidatastorage_dev.organisation O2 ON O2.parentOrgId=O1.id
+		INNER JOIN sentidatastorage_dev.organisation O3 ON O3.parentOrgId=O2.id
+		INNER JOIN sentidatastorage_dev.organisation O4 ON O4.parentOrgId=O3.id
+		WHERE O1.parentOrgId=0
+	) o4
+	ORDER BY niveau, id`
+	let rs = await mysqlConn.query(select, [])
+	if (rs[0].length === 0) {
+		return false
+	}
+	let result = []
+
+	rs[0].shift()
+
+
+	// Alternativ til Promise.all
+	await rs[0].reduce(async (promise, row) => {
+		// This line will wait for the last async function to finish.
+		// The first iteration uses an already resolved Promise
+		// so, it will immediately continue.
+		await promise;
+		let org = await entity.getDbOrganisationById(row.id)
+		console.log(org)
+
+		let aclOrgResources = await entity.createAclOrgResources(org)
+
+		// Register ACL ORG as resource
+		let aclOrgResource = await aclClient.registerResource(aclOrgResources.aclorg.uuid, ResourceType.aclorg)
+		// Register org as entity
+		let orgEntity = await aclClient.registerEntity(org.uuid)
+		// Register org as resource under ACL ORG
+		let orgResource = await aclClient.registerResource(org.uuid, ResourceType.org)
+		if (orgEntity.uuid !== orgResource.uuid) {
+			console.log('Something went really wrong...')
+		}
+		await aclClient.addResourceToParent(orgResource.uuid, aclOrgResource.uuid)
+	
+		// Register aclOrgResources and add them to ORG
+		await Promise.all(Object.entries(aclOrgResources).map(async ([, aclResource]) => {
+			if (aclResource.type !== 1 && aclResource.type !== 3) {
+				await aclClient.registerResource(aclResource.uuid, aclResource.type)
+				await aclClient.addResourceToParent(aclResource.uuid, orgResource.uuid)
+			}
+		}))
+		// Check for ORGROLES or create some...
+		let orgRoles = await entity.createAclOrganisationRoles(org.id)
+		// console.log(orgRoles)
+		await Promise.all(orgRoles.map(async (orgRole) => {
+			// Register role as entity under org
+			await aclClient.registerEntity(orgRole.aclUUID)
+			await aclClient.addEntityToParent(orgRole.aclUUID, orgEntity.uuid)
+			// Add initial privileges for role on org->aclResources
+			await Promise.all(Object.entries(orgRole.internal.initialPrivileges).map(async ([key, privileges]) => {
+				let p = await aclClient.addPrivileges(orgRole.aclUUID, aclOrgResources[key].uuid, privileges)
+				//console.log(orgRole.uuid, aclOrgResources[key].uuid, privileges, p)
+			}))
+		}))
+
+
+		result.push(org)
+	}, Promise.resolve())
+
+	res.status(200).json(result)
+})
+
+router.get('/v2/internal/users/allaclfix', async (req, res) => {
+	let lease = await authClient.getLease(req)
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+
+	let select = `SELECT u.uuid as userUUID, ao.uuid as orgUUID, aor.uuid as roleUUID 
+	FROM user u
+		INNER JOIN aclOrganisationResource ao ON ao.orgId = u.orgId
+		INNER JOIN aclResource ares ON ao.resourceId = ares.id AND ares.type = 5
+		INNER JOIN aclOrganisationRole aor ON aor.orgId = u.orgId AND aor.roleId = u.roleId`
+	let rs = await mysqlConn.query(select, [])
+	if (rs[0].length === 0) {
+		return false
+	}
+	let result = []
+
+	// rs[0].shift()
+	// Alternativ til Promise.all
+	await rs[0].reduce(async (promise, row) => {
+		// This line will wait for the last async function to finish.
+		// The first iteration uses an already resolved Promise
+		// so, it will immediately continue.
+		await promise;
+		// let org = await entity.getDbOrganisationById(row.id)
+		console.log(row)
+
+		// Register user
+		await aclClient.registerEntity(row.userUUID)
+		// Add user to Role
+		await aclClient.addEntityToParent(row.userUUID, row.roleUUID)
+		// Register user as resource
+		await aclClient.registerResource(row.userUUID, ResourceType.user)
+		// Add resource to organisation
+		await aclClient.addResourceToParent(row.userUUID, row.orgUUID)
+		// Give user permission to read, edit and delete there own resource
+		await aclClient.addPrivileges(row.userUUID, row.userUUID, [Privilege.user.read, Privilege.user.modify, Privilege.user.delete])
+
+		result.push(row)
+	}, Promise.resolve())
 	res.status(200).json(result)
 })
 
@@ -535,7 +790,7 @@ async function localCreateRoles() {
 	})
 	
 	await entity.dbSaveRole({ name: "System User", type: 1, priority: 0, "internal": { initialPrivileges: { aclorg: arrPrivileges } } })
-	await entity.dbSaveRole({ name: "Super User", type: 2, priority: 10, "internal": { initialPrivileges: { appui: ["waterworks.data","waterworks.admin"], aclorg: arrPrivileges } } })
+	await entity.dbSaveRole({ name: "Super User", type: 2, priority: 10, "internal": { initialPrivileges: { appui: ["waterworks.data", "waterworks.admin"], aclorg: arrPrivileges } } })
 	let aclorgPrivileges = [
 		"org.read",
 		"org.create",
