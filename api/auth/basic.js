637bca9079e9548fafda749dd8449085f4584d5c
Andrei Tudor <andrei.tudor27@gmail.com>
2021-09-26 20:51:59 +0200

    Basic Login Errors

--- api/auth/basic.js
+++ api/auth/basic.js
@@ -12,14 +12,30 @@ router.post('/v2/auth/basic', async (req, res) => {
 		}
 	}
 	if (credentials.username !== false && credentials.password !== false) {
-		let lease = await auth.login(credentials.username, credentials.password)    
+		let lease = await auth.login(credentials.username, credentials.password)
 		if (lease !== false) {
+			if (lease.error) {
+				res.status(400).json(lease)
+			}
 			res.status(200).json(lease)
 		} else {
 			res.status(404).json(lease)
 		}
 	} else {
-		res.status(400).json('')
+		if (credentials.username === false) {
+			res.status(400).json({
+				error: "login.missingPassword"
+			})
+		}
+		if (credentials.password === false) {
+
+			res.status(400).json({
+				error: "login.missingPassword"
+			})
+		}
+		res.status(500).json({
+			error: "login.unknownError"
+		})
 	}
 })
 module.exports = router
