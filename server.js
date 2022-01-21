d7e58a0f96e3759f3c2c5069778ee70e46d522d3
Mikkel Broberg <mb@webhouse.dk>
2022-01-21 16:22:56 +0100

    Payload size fix

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ const port = process.env.NODE_PORT || 3024
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 app.use(cors())
 
