6f293bff8c8e547cd32a3a744571731a5fe4b9c5
Mikkel Broberg <mb@webhouse.dk>
2022-01-24 13:56:51 +0100

    more limit fix

--- server.js
+++ server.js
@@ -38,8 +38,6 @@ const acltest = require('./api/acl/testResources')
 const port = process.env.NODE_PORT || 3024
 
 app.use(helmet())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
